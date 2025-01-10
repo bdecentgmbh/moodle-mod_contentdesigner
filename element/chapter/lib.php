22f9d9133182f05a16440672091fd6495272c328
abinesh-lmsace <abinesh.s@lmsace.com>
2025-01-10 12:02:30 +0530

    Bugfix: MOODLE_INTERNAL Definition added. - CD-351

--- element/chapter/lib.php
+++ element/chapter/lib.php
@@ -22,6 +22,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+defined('MOODLE_INTERNAL') || die();
+
  /**
   * Update the completion of chapter and generate the progress bar for the current module contents.
   *
