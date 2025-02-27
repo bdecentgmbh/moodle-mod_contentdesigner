9495f62976707f9f3c04728ab93879c4fff50419
bdecent <hello@bdecent.de>
2025-02-27 15:35:11 +0100

    Remove custom completion rules

--- lib.php
+++ lib.php
@@ -145,7 +145,7 @@ function contentdesigner_supports($feature) {
         case FEATURE_COMPLETION_TRACKS_VIEWS:
             return true;
         case FEATURE_COMPLETION_HAS_RULES:
-            return true;
+            return false;
         case FEATURE_GRADE_HAS_GRADE:
             return false;
         case FEATURE_GRADE_OUTCOMES:
